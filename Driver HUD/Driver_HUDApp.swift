3b32343ca6997bff7a80a3bd6b0011bf60a1c5cc
Aditya Khadye <adityakhadye0406@gmail.com>
2025-07-30 14:16:51 -0400

    Initial commit for GlassDriveHUD demo

---
+++ Driver HUD/Driver_HUDApp.swift
@@ -0,0 +1,33 @@
+//
+//  Driver_HUDApp.swift
+//  Driver HUD
+//
+//  Created by Dev Aditya on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Driver_HUDApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
---
+++ Driver HUDTests/Driver_HUDTests.swift
@@ -0,0 +1,17 @@
+//
+//  Driver_HUDTests.swift
+//  Driver HUDTests
+//
+//  Created by Dev Aditya on 7/23/25.
+//
+
+import Testing
+@testable import Driver_HUD
+
+struct Driver_HUDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
